4a8b6d9272c869e6c9b1af253a2cabda8da709b7
Akarsh <akarsh.srivastava@differenzsystem.com>
2025-10-15 10:12:57 +0530

    minor changes in frontend styling

--- Frontend/src/Pages/Auth/Login/Login.jsx
+++ Frontend/src/Pages/Auth/Login/Login.jsx
@@ -45,19 +45,6 @@ const Login = () => {
                     transition: Bounce,
                 });
             }
-            else {
-                toast.error("Invalid credentials or User doesn't exists!", {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: false,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                    transition: Bounce,
-                });
-            }
         }
         catch (err) {
             toast.error("Invalid credentials or User doesn't exists!", {
